ffc406d6a50e7364127a63b8f8fb0f5a136d0935
Aleksandr Zadorozhnyi <alexand98@yandex.ru>
2023-08-23 20:03:26 +0300

    working variant

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -8,13 +8,26 @@ def get_kit_body(name):
     current_body["name"] = name
     # возвращается новый словарь с нужным значением name
     return current_body
-def test_create_kit_1_letter_in_name_get_success_response():
-    # В переменную user_body сохраняется обновленное тело запроса с именем “Аа”
-    user_body = get_kit_body("а")
-    # В переменную user_response сохраняется результат запроса на создание пользователя
-    user_response = sender_stand_request.post_new_kit(user_body)
 
+def get_new_user_token():
+    return sender_stand_request.post_new_user(data.user_body).json()['authToken']
+
+def get_headers_for_post_new_kit():
+    kit_headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {get_new_user_token()}"
+    }
+    return kit_headers
+
+def post_new_kit_for_auth_user(body):
+    headers = get_headers_for_post_new_kit()
+    return sender_stand_request.post_new_kit(body, headers)
+def test_create_kit_1_letter_in_name_get_success_response():
+    # В переменную user_body сохраняется обновленное тело запроса с именем “а”
+    user_body = get_kit_body("а")
+    # В переменную user_response сохраняется результат запроса на создание нового набора
+    user_response = post_new_kit_for_auth_user(user_body)
     # Проверяется, что код ответа равен 201
     assert user_response.status_code == 201, "Status code is not 201"
-    # Проверяется, что в ответе есть поле authToken, и оно не пустое
+    # Проверяется, что в ответе есть поле name, и оно равно полю name в запросе
     assert user_response.json()["name"] == user_body["name"]
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -7,20 +7,7 @@ def post_new_user(body):
                          json=body,  # тут тело
                          headers=data.headers)  # а здесь заголовки
 
-response = post_new_user(data.user_body)
-print(response.status_code)
-print(response.json())
-
-def post_new_kit(body):
-    post_new_user_response = post_new_user(data.user_body).json()
-    authToken = post_new_user_response['authToken']
-    kit_headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {authToken}"
-    }
+def post_new_kit(body, kit_headers):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_KIT_PATH,  # подставляем полный url
                          json=body,  # тут тело
                          headers=kit_headers)  # а здесь заголовки
-response2 = post_new_kit(data.kit_body)
-print(response2.status_code)
-print(response2.json())
