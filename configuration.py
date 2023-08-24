d0bde80698de0650f60bf6a0ba9b300897afff65
Aleksandr Zadorozhnyi <alexand98@yandex.ru>
2023-08-24 23:01:56 +0300

    add readme.md

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 
-URL_SERVICE = "https://a39966a6-7f6c-45e9-8982-fd0f4dfe9016.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://cda7fd7d-554b-47e0-a25d-d75405ea2e55.serverhub.praktikum-services.ru"
 CREATE_USER_PATH = "/api/v1/users"
 CREATE_KIT_PATH = "/api/v1/kits"
--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -31,3 +31,96 @@ def test_create_kit_1_letter_in_name_get_success_response():
     assert user_response.status_code == 201, "Status code is not 201"
     # Проверяется, что в ответе есть поле name, и оно равно полю name в запросе
     assert user_response.json()["name"] == user_body["name"]
+
+def test_create_kit_511_letter_in_name_get_success_response():
+    test_string = 'abcd' * 127 + 'abc'
+    # В переменную user_body сохраняется обновленное тело запроса
+    user_body = get_kit_body(test_string)
+    # В переменную user_response сохраняется результат запроса на создание нового набора
+    user_response = post_new_kit_for_auth_user(user_body)
+    # Проверяется, что код ответа равен 201
+    assert user_response.status_code == 201, "Status code is not 201"
+    # Проверяется, что в ответе есть поле name, и оно равно полю name в запросе
+    assert user_response.json()["name"] == user_body["name"]
+
+def test_create_kit_0_letter_in_name_get_negative_response():
+    # В переменную user_body сохраняется обновленное тело запроса с именем “”
+    user_body = get_kit_body("")
+    # В переменную user_response сохраняется результат запроса на создание нового набора
+    user_response = post_new_kit_for_auth_user(user_body)
+    # Проверяется, что код ответа равен 400
+    assert user_response.status_code == 400, "Status code is not 400"
+
+def test_create_kit_512_letter_in_name_get_negative_response():
+    test_string = 'abcd' * 128
+    # В переменную user_body сохраняется обновленное тело запроса
+    user_body = get_kit_body(test_string)
+    # В переменную user_response сохраняется результат запроса на создание нового набора
+    user_response = post_new_kit_for_auth_user(user_body)
+    # Проверяется, что код ответа равен 400
+    assert user_response.status_code == 400, "Status code is not 400"
+
+def test_create_kit_english_letters_in_name_get_success_response():
+    # В переменную user_body сохраняется обновленное тело запроса
+    user_body = get_kit_body("QWErty")
+    # В переменную user_response сохраняется результат запроса на создание нового набора
+    user_response = post_new_kit_for_auth_user(user_body)
+    # Проверяется, что код ответа равен 201
+    assert user_response.status_code == 201, "Status code is not 201"
+    # Проверяется, что в ответе есть поле name, и оно равно полю name в запросе
+    assert user_response.json()["name"] == user_body["name"]
+
+def test_create_kit_russian_letters_in_name_get_success_response():
+    # В переменную user_body сохраняется обновленное тело запроса
+    user_body = get_kit_body("Мария")
+    # В переменную user_response сохраняется результат запроса на создание нового набора
+    user_response = post_new_kit_for_auth_user(user_body)
+    # Проверяется, что код ответа равен 201
+    assert user_response.status_code == 201, "Status code is not 201"
+    # Проверяется, что в ответе есть поле name, и оно равно полю name в запросе
+    assert user_response.json()["name"] == user_body["name"]
+
+def test_create_kit_special_characters_in_name_get_success_response():
+    # В переменную user_body сохраняется обновленное тело запроса
+    user_body = get_kit_body('"№%@",')
+    # В переменную user_response сохраняется результат запроса на создание нового набора
+    user_response = post_new_kit_for_auth_user(user_body)
+    assert user_response.status_code == 201, "Status code is not 201"
+    # Проверяется, что в ответе есть поле name, и оно равно полю name в запросе
+    assert user_response.json()["name"] == user_body["name"]
+
+def test_create_kit_whitespaces_in_name_get_success_response():
+    # В переменную user_body сохраняется обновленное тело запроса
+    user_body = get_kit_body(" Человек и КО ")
+    # В переменную user_response сохраняется результат запроса на создание нового набора
+    user_response = post_new_kit_for_auth_user(user_body)
+    # Проверяется, что код ответа равен 201
+    assert user_response.status_code == 201, "Status code is not 201"
+    # Проверяется, что в ответе есть поле name, и оно равно полю name в запросе
+    assert user_response.json()["name"] == user_body["name"]
+
+def test_create_kit_numbers_in_name_get_success_response():
+    # В переменную user_body сохраняется обновленное тело запроса
+    user_body = get_kit_body("123")
+    # В переменную user_response сохраняется результат запроса на создание нового набора
+    user_response = post_new_kit_for_auth_user(user_body)
+    # Проверяется, что код ответа равен 201
+    assert user_response.status_code == 201, "Status code is not 201"
+    # Проверяется, что в ответе есть поле name, и оно равно полю name в запросе
+    assert user_response.json()["name"] == user_body["name"]
+
+def test_create_kit_without_parametr_get_negative_response():
+    # В переменную user_body сохраняется пустой dict
+    user_body = {}
+    # В переменную user_response сохраняется результат запроса на создание нового набора
+    user_response = post_new_kit_for_auth_user(user_body)
+    # Проверяется, что код ответа равен 400
+    assert user_response.status_code == 400, "Status code is not 400"
+
+def test_create_kit_other_type_of_parametr_in_name_get_negative_response():
+    # В переменную user_body сохраняется обновленное тело запроса
+    user_body = get_kit_body(123)
+    # В переменную user_response сохраняется результат запроса на создание нового набора
+    user_response = post_new_kit_for_auth_user(user_body)
+    # Проверяется, что код ответа равен 400
+    assert user_response.status_code == 400, "Status code is not 400"
